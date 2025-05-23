b68a3061c27c4b5bf602ce894beee7d96dc69527
amirul.azreem <amirul.azreem@tranglo.com>
2025-05-23 16:16:02 +0800

    add config supabase

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["donate.sols.foundation", "drive.google.com"],
+    domains: [
+      "donate.sols.foundation",
+      "drive.google.com",
+      "xhpugefhcgqjkanhmanu.supabase.co",
+    ],
   },
 };
 
